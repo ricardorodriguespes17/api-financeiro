15e92118ab094f6d3cee0daca02096f69f802db8
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-16 18:09:10 -0300

    :sparkles: feat: add user route to update user data

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,5 +6,6 @@ const userController = new UserController()
 
 router.get("/users/:id", userController.getUserById)
 router.post("/users", userController.createUser)
+router.put("/users/:id", userController.updateUser)
 
 export default router
