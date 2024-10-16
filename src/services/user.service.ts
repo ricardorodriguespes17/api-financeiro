dbb0709ab566cb59097890b5dab052aec93ffb2d
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-16 16:23:24 -0300

    :sparkles: feat: create user service and define user type

---
+++ src/services/user.service.ts
@@ -0,0 +1,44 @@
+import { UserType } from "../@types/user.types"
+import UserRepository from "../repositories/user.repository"
+
+class UserService {
+  private userRepository = new UserRepository()
+
+  async getAllUsers() {
+    return this.userRepository.findAll()
+  }
+
+  async getUserById(id: string) {
+    const user = await this.userRepository.findById(id)
+
+    if(!user) {
+      throw new Error("User not found")
+    }
+
+    return user
+  }
+
+  async getUserByEmail(email: string) {
+    const user = await this.userRepository.findByEmail(email)
+
+    if(!user) {
+      throw new Error("User not found")
+    }
+
+    return user
+  }
+
+  async createUser(data: UserType) {
+    return this.userRepository.create(data)
+  }
+
+  async updateUser(id: string, data: UserType) {
+    return this.userRepository.update(id, data)
+  }
+
+  async deleteUser(id: string) {
+    return this.userRepository.delete(id)
+  }
+}
+
+export default UserService
