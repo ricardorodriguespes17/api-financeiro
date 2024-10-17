8600a09e6378fa95e105c3c6e364cfaaebd60e68
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-16 23:41:50 -0300

    :bug: fix: resolve user controller error

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,12 +2,14 @@ import { Request, Response } from "express"
 import UserService from "../services/user.service"
 import { CreateUserType, UpdateUserType } from "../@types/user.types"
 
+const userService = new UserService()
+
 class UserController {
-  private userService = new UserService()
 
   async getUserById(req: Request, res: Response) {
     try {
-      const user = await this.userService.getUserById(req.params.id)
+
+      const user = await userService.getUserById(req.params.id)
       res.status(200).json(user)
     } catch (error) {
       const message = (error as Error).message
@@ -32,9 +34,11 @@ class UserController {
     }
 
     try {
-      await this.userService.createUser(data)
+
+      await userService.createUser(data)
       res.status(201).json({ message: "User created successfully" })
     } catch (error) {
+      console.log(error)
       res.status(500).json({ message: error })
     }
   }
@@ -50,7 +54,8 @@ class UserController {
     }
 
     try {
-      await this.userService.updateUser(id, data)
+
+      await userService.updateUser(id, data)
       res.status(201).json({ message: "User created successfully" })
     } catch (error) {
       res.status(500).json({ message: error })
