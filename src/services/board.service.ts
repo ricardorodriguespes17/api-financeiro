ca0675655162ee50634fbda8ad68859892fd0b26
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-17 15:57:55 -0300

    :sparkles: feat: implement board service

---
+++ src/services/board.service.ts
@@ -0,0 +1,38 @@
+import { CreateBoardType, UpdateBoardType } from "../@types/board.types"
+import BoardRepository from "../repositories/board.repository"
+
+class BoardService {
+  private boardRepository = new BoardRepository()
+
+  async getAllBoards(userId: string) {
+    return this.boardRepository.findAll(userId)
+  }
+
+  async getBoardById(id: string) {
+    const board = await this.boardRepository.findById(id)
+
+    if(!board) {
+      throw new Error("Board not found")
+    }
+
+    return board
+  }
+
+  async createBoard(data: CreateBoardType) {
+    return this.boardRepository.create(data)
+  }
+
+  async updateBoard(id: string, data: UpdateBoardType) {
+    await this.getBoardById(id)
+
+    return this.boardRepository.update(id, data)
+  }
+
+  async deleteBoard(id: string) {
+    await this.getBoardById(id)
+
+    return this.boardRepository.delete(id)
+  }
+}
+
+export default BoardService
