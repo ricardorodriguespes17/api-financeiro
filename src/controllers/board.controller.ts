781903b0707329e3bfd0f3071632880bfa75a361
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-17 17:10:02 -0300

    :sparkles: feat: implement board controller

---
+++ src/controllers/board.controller.ts
@@ -0,0 +1,93 @@
+import { Request, Response } from "express"
+import BoardService from "../services/board.service"
+import { CreateBoardType, UpdateBoardType } from "../@types/board.types"
+
+class BoardController {
+  private boardService: BoardService
+
+  constructor(boardService?: BoardService) {
+    this.boardService = boardService || new BoardService()
+  }
+
+  async getAllBoards(req: Request, res: Response) {
+    const { userId } = req.body
+
+    try {
+      const boards = await this.boardService.getAllBoards(userId)
+      res.status(200).json(boards)
+    } catch (error) {
+      res.status(500).json({ message: "Internal Error" })
+    }
+  }
+
+  async getBoardById(req: Request, res: Response) {
+    const { id } = req.params
+
+    try {
+      const board = await this.boardService.getBoardById(id)
+      res.status(200).json(board)
+    } catch (error) {
+      res.status(404).json({ message: "Board not found" })
+    }
+  }
+
+  async createBoard(req: Request, res: Response) {
+    const { id, userId } = req.body
+
+    const data: CreateBoardType = {
+      id,
+      userId,
+      initialValue: 0
+    }
+
+    try {
+      await this.boardService.createBoard(data)
+      res.status(201).json({ message: "Board created successfully" })
+    } catch (error) {
+      res.status(500).json({ message: "Internal Error" })
+    }
+  }
+
+  async updateBoard(req: Request, res: Response) {
+    const id = req.params.id
+    const { initialValue, userId } = req.body
+
+    const data: UpdateBoardType = {
+      userId,
+      initialValue
+    }
+
+    try {
+      await this.boardService.updateBoard(id, data)
+      res.status(200).json({ message: "Board updated successfully" })
+    } catch (error) {
+      const message = (error as Error).message
+
+      if (message === "Board not found") {
+        res.status(404).json({ message })
+      } else {
+        res.status(500).json({ message: "Internal Error" })
+      }
+
+    }
+  }
+
+  async deleteBoard(req: Request, res: Response) {
+    const id = req.params.id
+
+    try {
+      await this.boardService.deleteBoard(id)
+      res.status(200).json({ message: "Board deleted successfully" })
+    } catch (error) {
+      const message = (error as Error).message
+
+      if (message === "Board not found") {
+        res.status(404).json({ message })
+      } else {
+        res.status(500).json({ message: "Internal Error" })
+      }
+    }
+  }
+}
+
+export default BoardController
