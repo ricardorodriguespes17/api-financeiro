e20a6e70c8f2a776ebed64aa34a0dd4be9c826d2
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-16 23:41:24 -0300

    :bug: fix: resolve user validator error

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -1,22 +1,22 @@
 import { NextFunction, Request, Response } from "express"
 import Joi from "joi"
 
+const createUserSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  birthdate: Joi.string().required()
+})
+
+const updateUserSchema = Joi.object({
+  name: Joi.string().min(3),
+  email: Joi.string().email(),
+  birthdate: Joi.string()
+})
+
 class UserValidator {
-  private createUserSchema = Joi.object({
-    name: Joi.string().min(3).required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required(),
-    birthdate: Joi.string().required()
-  })
-
-  private updateUserSchema = Joi.object({
-    name: Joi.string().min(3),
-    email: Joi.string().email(),
-    birthdate: Joi.string()
-  })
-
   async createUser(req: Request, res: Response, next: NextFunction) {
-    const { error } = this.createUserSchema.validate(req.body)
+    const { error } = createUserSchema.validate(req.body)
 
     if (error) {
       res.status(400).json({ message: error.details[0].message })
@@ -26,9 +26,10 @@ class UserValidator {
   }
   
   async updateUser(req: Request, res: Response, next: NextFunction) {
-    const { error } = this.updateUserSchema.validate(req.body)
+    const { error } = updateUserSchema.validate(req.body)
 
-    if (error) {
+    console.log(error)
+    if (error?.details) {
       res.status(400).json({ message: error.details[0].message })
     } else {
       next()
