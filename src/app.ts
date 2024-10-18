8b76a8c4cb65bd4ad075cb8b27cddc0a2295c801
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-18 14:07:39 -0300

    :sparkles: feat: create root route in app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import userRouter from "./routes/user.routes"
 import boardRouter from "./routes/board.routes"
 import transferenceRouter from "./routes/transference.routes"
 
 const app = express()
 app.use(express.json())
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Bem-vindo à API Financeiro")
+})
+
 app.use(userRouter)
 app.use(boardRouter)
 app.use(transferenceRouter)
+
 
 export default app
