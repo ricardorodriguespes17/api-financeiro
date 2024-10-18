bcd0f47ae7b450ab3f2f2554165a9867e4958387
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-18 01:41:38 -0300

    :recycle: refactor: add auth middleware in transference routes

--- src/routes/transference.routes.ts
+++ src/routes/transference.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express"
 import TransferenceController from "../controllers/transference.controller"
+import authMiddleware from "../middlewares/authMiddlewares"
 
 const transferenceRouter = Router()
 const transferenceController = new TransferenceController()
 
-transferenceRouter.get("/transferences/:boardId", transferenceController.getTransferencesByBoard)
-transferenceRouter.post("/transferences", transferenceController.createTransference)
-transferenceRouter.put("/transferences/:id", transferenceController.updateTransference)
-transferenceRouter.delete("/transferences/:id", transferenceController.deleteTransference)
+transferenceRouter.get("/transferences/:boardId", authMiddleware, transferenceController.getTransferencesByBoard)
+transferenceRouter.post("/transferences", authMiddleware, transferenceController.createTransference)
+transferenceRouter.put("/transferences/:id", authMiddleware, transferenceController.updateTransference)
+transferenceRouter.delete("/transferences/:id", authMiddleware, transferenceController.deleteTransference)
 
 export default transferenceRouter
