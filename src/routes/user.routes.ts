a7abf4129168e333b1a97315f0c61b69e036377f
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-18 14:07:04 -0300

    :recycle: refactor: add auth middleware in user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express"
 import UserController from "../controllers/user.controller"
 import UserValidator from "../validators/user.validator"
+import authMiddleware from "../middlewares/authMiddlewares"
 
 const router = Router()
 const userController = new UserController()
 const userValidator = new UserValidator()
 
-router.get("/users/:id", userController.getUserById)
+router.get("/users/:id", authMiddleware, userController.getUserById)
 router.post("/users", userValidator.createUser, userController.createUser)
-router.put("/users/:id", userValidator.updateUser, userController.updateUser)
+router.put("/users/:id", authMiddleware, userValidator.updateUser, userController.updateUser)
 
 export default router
