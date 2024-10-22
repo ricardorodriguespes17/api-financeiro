623e48dd7def2fe8f405d295f754852b9d4a940e
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-21 23:30:21 -0300

    :heavy_plus_sign: build: use cors

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express, { Request, Response } from "express"
 import userRouter from "./routes/user.routes"
 import boardRouter from "./routes/board.routes"
 import transferenceRouter from "./routes/transference.routes"
+import cors from "cors"
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Bem-vindo à API Financeiro")
