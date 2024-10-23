2a02063938407532c26f4c88f17a955b69c9d448
Ricardo Rodrigues <ricardor662@gmail.com>
2024-10-23 15:51:49 -0300

    :recycle: refactor: add authMiddleware in logout route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import AuthController from "../controllers/auth.controller"
+import authMiddleware from "../middlewares/authMiddlewares"
 
 const authRouter = Router()
 const authController = new AuthController()
 
 authRouter.post("/auth/login", authController.login)
-authRouter.post("/auth/logout", authController.logout)
+authRouter.post("/auth/logout", authMiddleware, authController.logout)
 authRouter.post("/auth/refresh", authController.refreshToken)
 
 export default authRouter
