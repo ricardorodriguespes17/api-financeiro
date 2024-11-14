a00dc398b76b600a98bf07d9fac853ab7f52c4be
Ricardo Rodrigues <ricardor662@gmail.com>
2024-11-13 22:41:19 -0300

    :sparkles: feat; create credit service

---
+++ src/services/credit.service.ts
@@ -0,0 +1,30 @@
+import { CreateCreditType, UpdateCreditType } from "../@types/credit.types"
+import { CreateUserType, UpdateUserType, UserType } from "../@types/user.types"
+import CreditRepository from "../repositories/credit.repository"
+import encryptPassword from "../utils/encryptPassword"
+
+class CreditService {
+  private creditRepository = new CreditRepository()
+
+  async getCreditsByUser(userId: string) {
+    return this.creditRepository.findByUser(userId)
+  }
+
+  async getCreditsByMonth(userId: string, month: string) {
+    return this.creditRepository.findByMonth(userId, month)
+  }
+
+  async createCredit(data: CreateCreditType) {
+    return this.creditRepository.create(data)
+  }
+
+  async updateUser(id: string, data: UpdateCreditType) {
+    return this.creditRepository.update(id, data)
+  }
+
+  async deleteUser(id: string) {
+    return this.creditRepository.delete(id)
+  }
+}
+
+export default CreditService
